aec814f77a1128ee3bcbd970757d12d92cd64702
Hellsingg <makspanama20@gmail.com>
2026-09-24 18:02:20 +0200

    Fix avatar

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
 async function loadProfile() {
-
     const res = await fetch("/api/profile");
     const user = await res.json();
 
-    avatar.src = "https://a.ppy.sh/38993897";
+    const avatar = document.getElementById("avatar");
+    avatar.src = `https://a.ppy.sh/${user.id}`;
 
-    username.textContent = user.username;
-    pp.textContent = Math.round(user.statistics.pp);
-
-    rank.textContent =
+    document.getElementById("username").textContent = user.username;
+    document.getElementById("pp").textContent = Math.round(user.statistics.pp);
+    document.getElementById("rank").textContent =
         "#" + user.statistics.global_rank.toLocaleString();
-
-    acc.textContent =
+    document.getElementById("acc").textContent =
         user.statistics.hit_accuracy.toFixed(2) + "%";
-
-    playcount.textContent =
+    document.getElementById("playcount").textContent =
         user.statistics.play_count.toLocaleString();
 }
 
